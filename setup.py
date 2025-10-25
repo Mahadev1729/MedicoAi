5a782a35ed65c1adcb681c7abe6059ddd9ff852b
Mahadev1729 <devcampus17@gmail.com>
2025-10-25 18:45:16 +0530

    Add requirements.txt, setup.py, and initial Jupyter notebook structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import find_packages, setup
+
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mahadev Athani",
+    author_email="athanimahadev348@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)

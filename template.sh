9d01f6b1079f9470ccc9c38871ec78ccbc94cecd
Mahadev1729 <devcampus17@gmail.com>
2025-10-25 18:03:23 +0530

    Add initial project structure and template script

---
+++ template.sh
@@ -0,0 +1,16 @@
+﻿# creating directory
+mkdir -p src
+mkdir -p research
+
+# create files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "success"
+
